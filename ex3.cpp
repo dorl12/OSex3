52b23d05202f5fa5130ec43cacb0a4b0145ffc65
Dor Levi <dorlev1204@gmail.com>
2022-05-31 09:36:06 +0300

    Initial commit

---
+++ ex3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>       // std::cout
+#include <thread>         // std::thread
+#include <mutex>          // std::mutex
+using namespace std;
+
+class BoundedQueue
+{
+    mutex mtx;
+};
+
